d72a4c9dca3cb12b1b1b007f4b6760dc5bb01b1a
Vladislav Ochirov <vladislavochirov@Vladislavs-MacBook-Pro.local>
2025-04-24 08:10:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Query
+from telethon.sync import TelegramClient
+import os
+
+app = FastAPI()
+
+# Получение переменных из окружения
+api_id = int(os.getenv("24053529"))
+api_hash = os.getenv("62b1700ac5981aece37890c54fbfdf1a")
+
+@app.get("/channel-posts")
+def get_posts(channel: str = Query(..., alias="channel")):
+    with TelegramClient("session", api_id, api_hash) as client:
+        try:
+            entity = client.get_entity(channel)
+            messages = client.iter_messages(entity, limit=20)
+            posts = []
+            for msg in messages:
+                if msg.message:
+                    posts.append({
+                        "id": msg.id,
+                        "text": msg.message,
+                        "views": msg.views,
+                        "date": str(msg.date),
+                        "link": f"https://t.me/{channel.replace('@','')}/{msg.id}"
+                    })
+            return posts
+        except Exception as e:
+            return {"error": str(e)}
