65da9f2fc7a8a9fb6d3e1bea103938711e306842
Vladislav Ochirov <vladislavochirov@Vladislavs-MacBook-Pro.local>
2025-04-24 09:40:53 +0300

    fix async context for Telethon

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from fastapi import FastAPI, Query
-from telethon.sync import TelegramClient
+from telethon import TelegramClient
 import os
 
 app = FastAPI()
 
-# Получение переменных из окружения
 api_id = int(os.getenv("TG_API_ID"))
 api_hash = os.getenv("TG_API_HASH")
 
+client = TelegramClient("session", api_id, api_hash)
+
+@app.on_event("startup")
+async def start():
+    await client.start()
+
 @app.get("/channel-posts")
-def get_posts(channel: str = Query(..., alias="channel")):
-    with TelegramClient("session", api_id, api_hash) as client:
-        try:
-            entity = client.get_entity(channel)
-            messages = client.iter_messages(entity, limit=20)
-            posts = []
-            for msg in messages:
-                if msg.message:
-                    posts.append({
-                        "id": msg.id,
-                        "text": msg.message,
-                        "views": msg.views,
-                        "date": str(msg.date),
-                        "link": f"https://t.me/{channel.replace('@','')}/{msg.id}"
-                    })
-            return posts
-        except Exception as e:
-            return {"error": str(e)}
+async def get_posts(channel: str = Query(..., alias="channel")):
+    try:
+        entity = await client.get_entity(channel)
+        messages = client.iter_messages(entity, limit=20)
+        posts = []
+        async for msg in messages:
+            if msg.message:
+                posts.append({
+                    "id": msg.id,
+                    "text": msg.message,
+                    "views": msg.views,
+                    "date": str(msg.date),
+                    "link": f"https://t.me/{channel.replace('@','')}/{msg.id}"
+                })
+        return posts
+    except Exception as e:
+        return {"error": str(e)}
